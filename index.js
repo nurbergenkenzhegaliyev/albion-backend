965bc29a353025593f54874a616d5602e7742303
nurbergenkenzhegaliyev <buenurbur@gmail.com>
2022-12-21 14:11:16 +0500

    server js

--- index.js
+++ index.js
@@ -1,43 +1,31 @@
-import dotenv from 'dotenv'
-dotenv.config()
-import express, { json } from 'express';
-import mongoose from 'mongoose';
-import authRouter from './router/authRouter.js';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import errorMiddleware from './middleware/error-middleware.js';
-import infoRouter from './router/infoRouter.js';
-import path from 'path'
+import dotenv from "dotenv";
+dotenv.config();
+import express, { json } from "express";
+import mongoose from "mongoose";
+import authRouter from "./router/authRouter.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import errorMiddleware from "./middleware/error-middleware.js";
+import infoRouter from "./router/infoRouter.js";
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-const __dirname = path.resolve();
-app.use(express.static(__dirname));
-app.use(express.static(path.resolve(__dirname, 'build')));
-
-app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-})
-
-
 
 app.use(json());
 app.use(cors());
 app.use(cookieParser());
-app.use("/auth", authRouter); 
+app.use("/auth", authRouter);
 app.use("/info", infoRouter);
 app.use(errorMiddleware);
 
-
-const start = async () => { 
-    try {
-        await mongoose.connect(process.env.DB_URL);
-        app.listen(PORT, ()=> console.log(`Server started at port ${PORT}`))
-    } catch (error) {
-        console.log(error)
-    }
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 start();
-
