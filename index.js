16890f583551b105dde88ac9affc44b168a4a312
nurbergenkenzhegaliyev <buenurbur@gmail.com>
2022-12-21 13:52:30 +0500

    check

--- index.js
+++ index.js
@@ -7,15 +7,25 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import errorMiddleware from './middleware/error-middleware.js';
 import infoRouter from './router/infoRouter.js';
+import path from 'path'
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+const __dirname = path.resolve();
+app.use(express.static(__dirname));
+app.use(express.static(path.resolve(__dirname, 'build')));
+
+app.get('*', (req,res) => {
+    res.sendFile(oath.join(__dirname, 'build', 'index.html'));
+})
+
+
 
 app.use(json());
 app.use(cors());
 app.use(cookieParser());
-app.use("/auth", authRouter);
+app.use("/auth", authRouter); 
 app.use("/info", infoRouter);
 app.use(errorMiddleware);
 
